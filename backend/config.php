8a9652b90da4d4ab5765787446cdd787fb942ded
tok2fatsu <mailit2moi@gmail.com>
2025-10-08 01:01:02 +0300

    fixing DB connection Error

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 // DB credentials
 $db_host = 'localhost';
-$db_port = '8001';
+$db_port = '3306';
 $db_name = 'curevedd_bookings';
 $db_user = 'curevedd_bookings';
 $db_pass = 'Qwerty@5936';
