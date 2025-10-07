90b43669bffcc49f1b73400f030141c7e2dc1e17
tok2fatsu <mailit2moi@gmail.com>
2025-10-07 19:15:57 +0300

    redis path inclusion

--- backend/api/contacts.php
+++ backend/api/contacts.php
@@ -2,6 +2,7 @@
 header('Content-Type: application/json');
 
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../includes/redis.php
 require_once __DIR__ . '/available_slots.php';
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
