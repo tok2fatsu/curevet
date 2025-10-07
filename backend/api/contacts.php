2f2b86f979b92c881264a241f61755717d3d549f
tok2fatsu <mailit2moi@gmail.com>
2025-10-07 18:58:17 +0300

    fixing file reference

--- backend/api/contacts.php
+++ backend/api/contacts.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 require_once __DIR__ . '/../config.php';
-
+require_once __DIR__ . '/available_slots.php';
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
